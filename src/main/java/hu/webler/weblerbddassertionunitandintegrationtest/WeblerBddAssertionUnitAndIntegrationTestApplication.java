eab9bdb76366d34cea6192a317e7cea5a1f57eb1
Szőke Roland <szokeroland0@gmail.com>
2024-03-23 11:55:21 +0100

    Initial commit

---
+++ src/main/java/hu/webler/weblerbddassertionunitandintegrationtest/WeblerBddAssertionUnitAndIntegrationTestApplication.java
@@ -0,0 +1,13 @@
+package hu.webler.weblerbddassertionunitandintegrationtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeblerBddAssertionUnitAndIntegrationTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeblerBddAssertionUnitAndIntegrationTestApplication.class, args);
+    }
+
+}
